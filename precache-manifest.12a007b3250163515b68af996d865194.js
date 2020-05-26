3cbec8dbb41fbb1da5b0035dd8e0e5410155dff3
nikolasalim <nikolasalim@gmail.com>
2020-05-26 13:11:09 +0200

    Updates

---
+++ precache-manifest.12a007b3250163515b68af996d865194.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9c7c780ece449c23a7984cc1096d80",
+    "url": "/nikolasalim.github.io/index.html"
+  },
+  {
+    "revision": "857cef5415996245aeba",
+    "url": "/nikolasalim.github.io/static/css/2.c2d0066e.chunk.css"
+  },
+  {
+    "revision": "be9ce3cafac09621abe3",
+    "url": "/nikolasalim.github.io/static/css/main.7c025a2e.chunk.css"
+  },
+  {
+    "revision": "857cef5415996245aeba",
+    "url": "/nikolasalim.github.io/static/js/2.f7a64df7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nikolasalim.github.io/static/js/2.f7a64df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9ce3cafac09621abe3",
+    "url": "/nikolasalim.github.io/static/js/main.1db1cde1.chunk.js"
+  },
+  {
+    "revision": "f955e1537f90189d550d",
+    "url": "/nikolasalim.github.io/static/js/runtime-main.ac5236a7.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/nikolasalim.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/nikolasalim.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/nikolasalim.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/nikolasalim.github.io/static/media/slick.f97e3bbf.svg"
+  }
+]);
