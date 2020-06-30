90c11c7d9f51c70dc19e3f699ad09363a6ff20d6
nikolasalim <nikolasalim@gmail.com>
2020-06-30 13:44:54 +0200

    add about info and set up scss

--- portfolio-gatsby/gatsby-config.js
+++ portfolio-gatsby/gatsby-config.js
@@ -1,18 +1,7 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- portfolio-gatsby/src/components/header.js
+++ portfolio-gatsby/src/components/header.js
@@ -1,6 +1,12 @@
 import { Link } from "gatsby"
 import React from "react"
 
-const Header = () => <header>this is the header</header>
+const Header = () => (
+  <header>
+    <div className="about">About</div>
+    <div className="smiley">:)</div>
+    <div className="about">Projects</div>
+  </header>
+)
 
 export default Header
