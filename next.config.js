920b1a1ad70c5515cd6db15f79a9eb04c6dda659
nikolasalim <nikolasalim@gmail.com>
2023-07-12 21:18:25 +0200

    Revert "attempt 2 to fix accessing public folder files"
    This reverts commit d8401315e8e378186bec90a14af6e2b6ec02460a.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production'
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  assetPrefix: isProd ? '/portfolio/' : '',
+  output: 'export'
+
 }
 
 module.exports = nextConfig
