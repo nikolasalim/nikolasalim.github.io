d8401315e8e378186bec90a14af6e2b6ec02460a
nikolasalim <nikolasalim@gmail.com>
2023-07-12 17:36:22 +0200

    attempt 2 to fix accessing public folder files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
+const isProd = process.env.NODE_ENV === 'production'
 
+const nextConfig = {
+  output: 'export',
+  assetPrefix: isProd ? '/portfolio/' : '',
 }
 
 module.exports = nextConfig
