b46ea71d141ac05acf8397094d3e1d4e0ed46db4
nikolasalim <nikolasalim@gmail.com>
2023-09-29 11:42:31 +0200

    tweak nav-menu font-sizes; create asset dir for fonts

--- app/components/nav-menu/nav-menu.tsx
+++ app/components/nav-menu/nav-menu.tsx
@@ -3,18 +3,19 @@
 import styles from './nav-menu.module.scss'
 import Link from 'next/link';
 import SoonTag from '@/app/components/soon-tag/soon-tag';
+import {usePathname} from "next/navigation";
 
 export default function NavMenu() {
+  const pathname = usePathname();
 
   return (
     <>
       <div className={styles.topNav}>
-        <Link href="/" className={styles.navLink}>
-          <span className={styles.navItem}>PROJECTS</span>
-          <span className={styles.soonTagWrapper}><SoonTag/></span>
+        <Link href="/" className={`${styles.navLink} ${pathname === '/about' ? styles.active : ''}`}>
+          <span className={styles.navItem}>ABOUT</span>
         </Link>
         <Link href="/" className={styles.navLink}>
-          <span className={styles.navItem}>ABOUT</span>
+          <span className={styles.navItem}>PROJECTS</span>
           <span className={styles.soonTagWrapper}><SoonTag/></span>
         </Link>
       </div>
