b7e0cad7c982c61add847e446b3697e457bf7e0f
nikolasalim <nikolasalim@gmail.com>
2023-10-13 16:25:24 +0200

    use css for toggling between mobile and desktop intro

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,52 +1,35 @@
-"use client"
-
 import styles from './about.module.scss';
-import {useEffect, useState} from "react";
+import Image from "next/image";
 
 export default function About() {
   const techStack = ['Angular', 'Typescript', 'HTML', 'SCSS', 'React', 'Next.js', 'Capacitor', 'Ionic', 'Playwright', 'Git']
   const designStack = ['Photoshop', 'Illustrator', 'Figma']
   const languages = ['English', 'Portuguese']
 
-  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 768)
-
-  useEffect(() => {
-    function handleResize() {
-      setIsMobile(window.innerWidth < 768);
-    }
-    window.addEventListener('resize', handleResize)
-  }, [])
-
   return (
     <main className={styles.content}>
-      <div className={styles.intro}>
-        {isMobile ? (
-            <>
-              <div className={styles.lineWrapper}><p>Hello – I'm Nikola Salim,</p></div>
-              <div className={styles.lineWrapper}><p>a front-end developer</p></div>
-              <div className={styles.lineWrapper}><p>on a quest to make</p></div>
-              <div className={styles.lineWrapper}><p>the internet look good</p></div>
-              <div className={styles.lineWrapper}><p>and applications feel right.</p></div>
-            </>
-        ) : (
-            <>
-              <div className={styles.lineWrapper}><p>Hello – I'm Nikola Salim,</p></div>
-              <div className={styles.lineWrapper}><p>a front-end developer on a quest</p></div>
-              <div className={styles.lineWrapper}><p>to make the internet look good</p></div>
-              <div className={styles.lineWrapper}><p>and applications feel right.</p></div>
-            </>
-        )}
-
+      <div className={`${styles.intro} ${styles.desktop}`}>
+        <div className={styles.lineWrapper}><p>Hello – I&apos;m Nikola Salim,</p></div>
+        <div className={styles.lineWrapper}><p>a front-end developer on a quest</p></div>
+        <div className={styles.lineWrapper}><p>to make the internet look good</p></div>
+        <div className={styles.lineWrapper}><p>and applications feel right.</p></div>
+      </div>
+      <div className={`${styles.intro} ${styles.mobile}`}>
+        <div className={styles.lineWrapper}><p>Hello – I&apos;m Nikola Salim,</p></div>
+        <div className={styles.lineWrapper}><p>a front-end developer</p></div>
+        <div className={styles.lineWrapper}><p>on a quest to make</p></div>
+        <div className={styles.lineWrapper}><p>the internet look good</p></div>
+        <div className={styles.lineWrapper}><p>and applications feel right.</p></div>
       </div>
       <div className={styles.additionalInfo}>
         <div className={styles.bio}>
           <p>As a developer, I have great interest in building products that resonate with people, bridging the gap between great designs and great implementation.</p>
           <br/>
-          <p>And as a person, I've always been curious about everything – which is especially handy when a big part of my job is learning new things every day.</p>
+          <p>And as a person, I&apos;ve always been curious about everything – which is especially handy when a big part of my job is learning new things every day.</p>
           <div className={styles.linksContainer}>
             <a href="https://drive.google.com/file/d/1c8VQi7TZLhkeXoW7kRo02-VT-m98fnVk/view?usp=sharing" target="_blank" className={styles.downloadBtn}>☞ Download CV</a>
-            <a href="https://github.com/nikolasalim" target="_blank"><img src="/assets/images/icon-github.svg" alt="github"/></a>
-            <a href="https://www.linkedin.com/in/nikolasalim/" target="_blank"><img src="/assets/images/icon-linkedin.svg" alt="linkedIn"/></a>
+            <a href="https://github.com/nikolasalim" target="_blank"><Image src="/assets/images/icon-github.svg" alt="github" height="24" width="24"/></a>
+            <a href="https://www.linkedin.com/in/nikolasalim/" target="_blank"><Image src="/assets/images/icon-linkedin.svg" alt="linkedIn" height="24" width="24"/></a>
           </div>
         </div>
         <div className={styles.stackContainer}>
